aa51bd2dd463307a95542835f54904c494c29292
Agustin Sanchez <trorik23@gmail.com>
2018-11-28 13:41:44 -0300

    anda todo

--- src/main.cpp
+++ src/main.cpp
@@ -18,8 +18,11 @@ QueueHandle_t queue2;
 float value1, value2;
 QueueHandle_t leds_sensor_1;
 
+char segundos = 5;
+float threshold = 0.66;
 void task_sensor_1(void *pvParameters)
 {
+    // Serial.println("Task 1");
     Sensor sensor(PIN_SENSOR_1);
     float value;
     int i;
@@ -40,6 +43,7 @@ void task_sensor_1(void *pvParameters)
 }
 void task_sensor_2(void *pvParameters)
 {
+    // Serial.println("Task 1");
     float value;
     Sensor sensor(PIN_SENSOR_2);
     int i;
@@ -60,30 +64,46 @@ void task_sensor_2(void *pvParameters)
 }
 
 void task_sender(void *pvParameters)
-{
+{   
+    // Serial.println("Task Sender");
     while (1)
     {
         xQueueReceive(queue1, &value1, portMAX_DELAY);
         xQueueReceive(queue2, &value2, portMAX_DELAY);
-        // Serial.print(value1);
-        // Serial.print("/");
-        // Serial.println(value2);
+        Serial.print(value1);
+        Serial.print("/");
+        Serial.println(value2);
     }
 }
 void task_IDLE(void *pvParameters)
 {
+    // Serial.println("Task IDLE");
     pinMode(PIN_LED_R, OUTPUT);
     pinMode(PIN_LED_Y, OUTPUT);
     pinMode(PIN_LED_G, OUTPUT);
     pinMode(PIN_RELAY, OUTPUT);
     unsigned long StartTime = 0;
     while (1)
-    {
+    {   
+        // Serial.println("IDLE");
         if (Serial.available() > 0)
         {
             // read the incoming byte:
-            Serial.println(Serial.read());
+            char c = Serial.read();
+            if(c == 'S'){
+                // Serial.print("Segundos ");
+                c = Serial.read();
+                segundos = c;
+                // Serial.println(c);
+            }
+            if(c == 'T'){
+                // Serial.print("Threshold ");
+                c = Serial.read();
+                threshold = c/100.0;
+                // Serial.println(c);
+            }
         }
+        //Leds de intensidad
         if (value1 > 0.05)
             digitalWrite(PIN_LED_G, LOW);
         else
@@ -94,19 +114,22 @@ void task_IDLE(void *pvParameters)
         else
             digitalWrite(PIN_LED_Y, HIGH);
 
-        if (value1 > 0.66){
-            digitalWrite(PIN_LED_R, LOW);
+        if (value1 > 0.66)
+            digitalWrite(PIN_LED_R, LOW);            
+        else
+            digitalWrite(PIN_LED_R, HIGH);
+
+        //relay
+        if (value1 > threshold){
             if(StartTime == 0)//not measuring
                 StartTime = millis();
             else{//was measuring
-                Serial.println((millis() - StartTime));
-                if((millis() - StartTime) > 5000){
-                    //5 seconds over the threshold
+                // Serial.println((millis() - StartTime));
+                if((millis() - StartTime) > 1000 * segundos){
                     digitalWrite(PIN_RELAY, LOW);                   
                 }
             }
         }else{
-            digitalWrite(PIN_LED_R, HIGH);
             StartTime = 0;
             digitalWrite(PIN_RELAY, HIGH);
         }
@@ -118,14 +141,15 @@ void setup()
 
     queue1 = xQueueCreate(1, sizeof(float));
     queue2 = xQueueCreate(1, sizeof(float));
+
     if(queue1 == NULL)
         Serial.println("Error al crear cola 1");
     if(queue2 == NULL)
         Serial.println("Error al crear cola 2");
-        
-    //xTaskCreate(task_IDLE, "task_IDLE", 128, NULL, 0, NULL);
-    xTaskCreate(task_sensor_1, "task_sensor_1", 256, NULL, 1, NULL);
-    xTaskCreate(task_sensor_2, "task_sensor_2", 256, NULL, 1, NULL);
+    
+    xTaskCreate(task_IDLE, "task_IDLE", 128, NULL, 0, NULL);
+    xTaskCreate(task_sensor_1, "task_sensor_1", 128, NULL, 1, NULL);
+    xTaskCreate(task_sensor_2, "task_sensor_2", 128, NULL, 1, NULL);
     xTaskCreate(task_sender, "task_sender", 128, NULL, 2, NULL);
 }
 
--- webserver/app.py
+++ webserver/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template                                        
 from flask_socketio import SocketIO, emit
 from threading import Thread                             
-import time   
+import time
 import eventlet
-eventlet.monkey_patch()                                                                
+eventlet.monkey_patch()
+import serial
+ser = serial.Serial('/dev/ttyACM0', 115200)                                                                
 
 app = Flask(__name__)
 app.debug = True                                                         
@@ -11,20 +13,34 @@ app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)                                                        
 thread = None                                                                   
 
-def background_thread():
-	i = 0                                                        
+def background_thread():                                                   
 	while True:
-		time.sleep(0.05)                                            
-		socketio.emit('value', {'value': i%10})
-		i = i + 1                       
+		time.sleep(0.001)
+		serial_line = ser.readline()
+		if(len(serial_line) < 9):
+			break
+		sensor1 = serial_line[:4]
+		sensor2 = serial_line[5:-2]
+		print(sensor1 + "/" + sensor2)
+		socketio.emit('value', {'value1': (float(sensor1)*10),
+		'value2': (float(sensor2)*10)})                   
 
 @app.route('/')                                                                 
 def index():
 	global thread
 	if thread is None:
 		thread = Thread(target=background_thread)
-		thread.start()                                                             
-	return render_template('index.html')        
+		thread.start()                                                     
+	return render_template('index.html')
+
+@socketio.on('parametros')
+def handle_parametros(json, methods=['GET', 'POST']):
+	print("Segundos", json['segundos'])
+	print("Threshold", json['threshold'])
+	ser.write(b'S')
+	ser.write([int(json['segundos'])])
+	ser.write(b'T')
+	ser.write([int(json['threshold'])])
 
 if __name__ == '__main__': 
-	socketio.run(app)	
+	socketio.run(app)
